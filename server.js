6d3776ec337a46405f740f48d55ca76e77fbdb36
Kelly Lima <keely06@live.com>
2019-06-19 21:00:13 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const controller = require('./PokemonsController')
+const params = require('params')
+const parametrosPermitidos = require('./parametrosPermitidos')
+const servidor = express()
+const PORT = 3000
+const logger = (request, response, next) => {
+  console.log(`${new Date().toISOString()} Request type: ${request.method} to ${request.originalUrl}`)
+
+  response.on('finish', () => {
+    console.log(`${response.statusCode} ${response.statusMessage};`)
+  })
+
+  next()
+}
+
+servidor.use(cors())
+servidor.use(bodyParser.json())
+servidor.use(logger)
+
+servidor.get('/', (request, response) => {
+  response.send('Olá, mundo!')
+})
+
+servidor.get('/pokemons', async (request, response) => {
+  controller.getAll()
+    .then(pokemons => response.send(pokemons))
+})
+
+servidor.get('/pokemons/:pokemonId', (request, response) => {
+  const pokemonId = request.params.pokemonId
+  controller.getById(pokemonId)
+    .then(pokemon => {
+      if(!pokemon){
+        response.sendStatus(404)
+      } else {
+        response.send(pokemon)
+      }
+    })
+    .catch(error => {
+      if(error.name === "CastError"){
+        response.sendStatus(400)
+      } else {
+        response.sendStatus(500)
+      }
+    })
+})
+
+servidor.patch('/pokemons/:id', (request, response) => {
+  const id = request.params.id
+  controller.update(id, params(request.body).only(parametrosPermitidos.update))
+    .then(pokemon => {
+      if(!pokemon) { response.sendStatus(404) }
+      else { response.send(pokemon) }
+    })
+    .catch(error => {
+      if(error.name === "MongoError" || error.name === "CastError"){
+        response.sendStatus(400)
+      } else {
+        response.sendStatus(500)
+      }
+    })
+})
+
+servidor.patch('/pokemons/treinar/:id', (request, response) => {
+  const id = request.params.id
+  controller.treinar(id, params(request.body).only(parametrosPermitidos.treinar))
+    .then(pokemon => {
+      if(!pokemon) { response.sendStatus(404) }
+      else { response.send(pokemon) }
+    })
+    .catch(error => {
+      if(error.name === "MongoError" || error.name === "CastError"){
+        response.sendStatus(400)
+      } else {
+        response.sendStatus(500)
+      }
+    })
+})
+
+servidor.post('/pokemons', (request, response) => {
+  controller.add(params(request.body).only(parametrosPermitidos.add))
+    .then(pokemon => {
+      const _id = pokemon._id
+      response.send(_id)
+    })
+    .catch(error => {
+      if(error.name === "ValidationError"){
+        response.sendStatus(400)
+      } else {
+        response.sendStatus(500)
+      }
+    })
+})
+
+
+servidor.listen(PORT)
+console.info(`Rodando na porta ${PORT}`)
